4bf8120cf14011202752065183ae2d288762e989
Sheroy <sheroygodrej.bha@my.bdsc.school.nz>
2026-08-21 10:42:27 +1200

    Importing all libraries needed for V2

--- Version 2.py
+++ Version 2.py
@@ -1,2 +1,8 @@
 '''This program is the second version of a screentime awareness app,
 it uses a Tkinter GUI, a ScreentimeEntry class, and saves data to a JSON file.'''
+
+from tkinter import *
+from tkinter import messagebox
+import json
+import os
+from datetime import datetime
